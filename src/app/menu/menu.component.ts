60704acfec5b0dc944138df533fb66dc0378406a
Gleb Strelchenko <glebspartan@gmail.com>
2017-07-28 16:28:10 +0300

    Add notifications

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,17 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import {MenuItem} from "primeng/primeng";
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
-
+ @Input() openedSidebar: boolean = false;
   constructor() { }
 
   items: MenuItem[];
+  open(event) {
+    let clickedComponent = event.target.closest('.nav-item');
+    let items = clickedComponent.parentElement.children;
 
+    for (let i = 0; i < items.length; i++) {
+      items[i].classList.remove('opened');
+    }
+    clickedComponent.classList.add('opened');
+  }
+
+  close(event) {
+    let clickedComponent = event.target;
+    let items = clickedComponent.parentElement.children;
+
+    for (let i = 0; i < items.length; i++) {
+      items[i].classList.remove('opened');
+    }
+  }
   ngOnInit() {
     this.items = [
       {label: 'Dashboard', routerLink: ['/dashboard']},
